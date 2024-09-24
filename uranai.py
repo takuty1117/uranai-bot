0f000fc1db3124b970a88c0e7863bc3adc737ec9
t.n <t.n@tnnonotopasokon.local>
2024-09-24 14:22:45 +0900

    Move uranai.py to project root

---
+++ uranai.py
@@ -0,0 +1,60 @@
+import os
+import discord
+import random
+import pandas as pd
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+from flask import Flask
+import threading
+
+# Flaskアプリケーションを定義
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return "Bot is running!"
+
+# Discordボットクラスの定義
+class MyBot(discord.Client):
+    async def on_ready(self):
+        print(f'Logged in as {self.user}')
+
+    async def on_message(self, message):
+        if message.author.bot:
+            return
+
+        if message.content == "今日の占い":
+            # 環境変数からGoogleサービスのJSONファイルのパスを取得
+            Auth = os.getenv('GOOGLE_APPLICATION_CREDENTIALS')  # 環境変数からパスを取得
+            scope = ['https://spreadsheets.google.com/feeds']
+            credentials = ServiceAccountCredentials.from_json_keyfile_name(Auth, scope)
+            client = gspread.authorize(credentials)
+
+            # スプレッドシートに接続
+            spreadsheet = client.open_by_key("1zIrZKLGHeYuhEHUvSn75qnZD5P7escBYZnL-3dvsNGs")
+            raw_data = spreadsheet.worksheet("シート1")
+            data = pd.DataFrame(raw_data.get_all_values())
+
+            # ランダムに占い結果を選ぶ
+            n = random.randint(0, len(data) - 1)
+            uranai = data.iloc[n, 0] + '\n' + data.iloc[n, 1]
+            await message.channel.send(uranai)
+
+# Discordボットを起動
+intents = discord.Intents.default()
+intents.message_content = True
+client = MyBot(intents=intents)
+
+# Flaskを別スレッドで実行する関数
+def run_flask():
+    port = int(os.environ.get("PORT", 5001))  # デフォルトでポート5001を使用
+    app.run(host="0.0.0.0", port=port)
+
+# アプリを起動する部分
+if __name__ == "__main__":
+    # Flaskを別スレッドで実行
+    flask_thread = threading.Thread(target=run_flask)
+    flask_thread.start()
+    
+    # Discordボットを実行
+    client.run(os.getenv('DISCORD_BOT_TOKEN'))
